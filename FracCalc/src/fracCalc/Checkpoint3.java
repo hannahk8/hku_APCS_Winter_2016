5a5ae5742efe6c99c79d30a6673dcd41f6159f38
hannahk8 <18kuh23@millshighsch.org>
2016-12-05 09:45:35 -0800

    cont
    cont

--- FracCalc/src/fracCalc/Checkpoint3.java
+++ FracCalc/src/fracCalc/Checkpoint3.java
@@ -1,3 +1,4 @@
+// this code receives input from the user and returns the answer to expressions
 package fracCalc;
 
 import java.util.Scanner;
@@ -9,6 +10,7 @@ public class Checkpoint3 {
 		Scanner userInput = new Scanner(System.in);
     	String inputEquation = "";
     	
+    	//continue prompting user for expression until the user types quit.
     	while(!inputEquation.equals("quit")){
     		System.out.println("Enter an equation: ");
     		inputEquation = userInput.nextLine();
@@ -36,6 +38,7 @@ public class Checkpoint3 {
     		}
     	}
     	
+    	//parse first and second parts(fractions into whole number, numerator and denominator)
     	int[] firstPartParsed = parseFraction(firstPart);
     	int[] secPartParsed = parseFraction(secPart);
     	
@@ -43,11 +46,13 @@ public class Checkpoint3 {
     }
     
     public static int[] parseFraction(String fraction){
+    	// initialize the whole, numerator, denominator and numAndDen values.
     	String whole = "0";
     	String numerator = "0";
     	String denominator = "1";
     	String numAndDen = "";
     	
+    	//split fraction into whole number and fraction
     	if(fraction.indexOf("_") != -1){
     		String[] splitFraction = fraction.split("_");
         	whole = splitFraction[0];
@@ -56,12 +61,13 @@ public class Checkpoint3 {
     		whole = fraction;
     	}
     	
+    	//split fraction into numerator and denominator
     	if(numAndDen.indexOf("/") != -1){
     		String[] splitNumandDen = numAndDen.split("/");
         	numerator = splitNumandDen[0];
         	denominator = splitNumandDen[1];
     	}
-    	
+    	//change all values from strings into integers
     	int integerWhole = Integer.parseInt(whole);
     	int integerNumerator = Integer.parseInt(numerator);
     	int integerDenominator = Integer.parseInt(denominator);
@@ -71,7 +77,32 @@ public class Checkpoint3 {
     	return fracArray;
     }
     
-    public static int[] improperFrac(int[] wholenumDenArray){
+    public static int[] improperFrac(int[] wholeNumDenArray){
+    	//initialize whole, numerator and denominator
+    	int whole = wholeNumDenArray[0];
+    	int numerator = wholeNumDenArray[1];
+    	int denominator = wholeNumDenArray[2];
     	
+    	//new improper fraction array
+    	int[] improperFraction = new int[2];
+    	improperFraction[0] = (whole*denominator) + numerator;
+    	improperFraction[1] = denominator;
+    	
+    	return improperFraction;
     }
+    
+    public static int[] addFrac(int[] firstFracToAdd, int[] secFracToAdd){
+    	// add numerators together, leave denominator the same.
+    	int[] sum = new int[2];
+    	sum[0] = firstFracToAdd[0] += secFracToAdd[0];
+    	sum[1] = firstFracToAdd[1];
+    	
+    	return sum;
+    }
+    
+    public static int[] multiplyFrac(int[] firstFracToMultiply, int[] secFracToMultiply){
+    	int[] product = new int[2];
+    	product[0] = 
+    	product[1] = 
+	}
 }
